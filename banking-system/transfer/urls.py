50413ebd38ac3c74e6cdc76de27644f679635164
Chaitanya Tyagi <chay2199@gmail.com>
2020-02-19 17:39:35 +0530

    Frontend updates

--- banking-system/transfer/urls.py
+++ banking-system/transfer/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from .views import transfer_fund
+from .views import foreign_fund
 
 urlpatterns = [
     url(r'^transfer/$', transfer_fund, name='fund_transfer'),
+    url(r'^ftransfer/$', foreign_fund, name='foreign_fund'),
 ]
--- banking-system/transfer/views.py
+++ banking-system/transfer/views.py
@@ -45,3 +45,36 @@ def transfer_fund(request):
         "form": form
     }
     return render(request, "transfer_fund.html", context)
+
+
+@login_required()
+def foreign_fund(request):
+    form = TransferForm(request.POST or None)
+    if form.is_valid():
+
+	    transfer = form.save(commit=False)
+	    transfer.sender = request.user
+	    receiver_data= transfer.receiver
+	    email=form.cleaned_data.get("receiver")
+	    transfer.receiver=email
+	    transfer.amount=form.cleaned_data.get("amount")
+	    transfer.save()
+	    if transfer.sender.account.balance<transfer.amount:
+	    	messages.success(
+            request, 'You cannot Withdraw more than your balance {} $.'.format(transfer.amount)
+        	)
+        	return redirect("home")
+	        # adds users deposit to balance.
+	    transfer.sender.account.balance -= transfer.amount
+	    transfer.receiver.account.balance += transfer.amount
+	    transfer.sender.account.save()
+	    transfer.receiver.account.save()
+	    messages.success(request, 'You Have Deposited {} $.'
+	                         .format(transfer.amount))
+	    return redirect("home")
+
+    context = {
+        "title": "Fund Transfer",
+        "form": form
+    }
+    return render(request, "foreign_fund.html", context)
